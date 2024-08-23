1bc5e4c7ed433d98f5b238f46882faaf693f3317
Quang Vu <astorre.vu@gmail.com>
2024-08-23 16:44:58 -0400

    Installed and configured ESlint. Installed dependencies based on old source code. Check the Old Package.json for more information about the removed and replaced packages.

---
+++ eslint.config.mjs
@@ -0,0 +1,47 @@
+// eslint.config.mjs
+
+import { ESLint } from "eslint";
+import typescriptPlugin from "@typescript-eslint/eslint-plugin";
+import angularPlugin from "@angular-eslint/eslint-plugin";
+import angularTemplatePlugin from "@angular-eslint/eslint-plugin-template";
+import typescriptParser from "@typescript-eslint/parser";
+import angularTemplateParser from "@angular-eslint/template-parser";
+
+/** @type {ESLint.FlatConfig[]} */
+const config = [
+  // TypeScript configuration
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      "@typescript-eslint": typescriptPlugin,
+      "@angular-eslint": angularPlugin,
+    },
+    rules: {
+      ...typescriptPlugin.configs.recommended.rules,
+      ...angularPlugin.configs.recommended.rules,
+    },
+  },
+  // Angular HTML template configuration
+  {
+    files: ["**/*.html"],
+    languageOptions: {
+      parser: angularTemplateParser,
+    },
+    plugins: {
+      "@angular-eslint/template": angularTemplatePlugin,
+    },
+    rules: {
+      ...angularTemplatePlugin.configs.recommended.rules,
+    },
+  },
+];
+
+export default config;
